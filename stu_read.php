71ddc7fbb591d08915c63e319a727cd5316f1331
Imran_mahmud <mahmudulimran396@gmail.com>
2026-05-19 16:25:54 +0600

    Update

--- stu_read.php
+++ stu_read.php
@@ -3,14 +3,33 @@
 include 'db.php';
 
 $sql = "SELECT * FROM studentdata1";
-
-$result = mysqli_query($conn,$sql);
+$result = mysqli_query($conn, $sql);
 
 $data = [];
 
-while($row = mysqli_fetch_assoc($result)){
-    $data[] = $row;
+if (mysqli_num_rows($result) > 0) {
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
+
+    echo json_encode($data);
+
+} else {
+
+    // fallback demo data (PHP array format)
+    echo json_encode([
+        [
+            "id" => 1,
+            "name" => "Mahmud",
+            "email" => "mahmud@gmail.com",
+            "department" => "CSE",
+            "semester" => "8th",
+            "github" => "https://github.com/mahmudul4747",
+            "cv" => "https://example.com/cv.pdf"
+        ]
+    ]);
+
 }
 
-echo json_encode($data ?? []);
 ?>
